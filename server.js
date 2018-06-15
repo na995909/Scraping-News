b6503f9b94cbf1192804e1b1c06ab365b92df49b
Nelly Ackerman <pavel.ack@gmail.com>
2018-06-15 15:56:26 -0400

    m

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Express static directory
 app.use(express.static("public"));
-var MONGODB_URI = process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
